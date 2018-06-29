351c13def7b94f756df6723ac92fa4b6fe4aa781
Robin Millette <robin@millette.info>
2018-06-28 21:36:53 -0400

    Comment next.config.js file.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 'use strict'
 
+/*
+Custom config for Next.js
+
+next-build-id is used to display the current commit hash
+in the exported website footer.
+
+withProgressBar adds a progress bar to webpack builds.
+*/
+
 const nextBuildId = require('next-build-id')
 const withProgressBar = require('next-progressbar')
 
