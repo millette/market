2d662926ad35f84bfb97e302186782896e613cb7
Robin Millette <robin@millette.info>
2018-06-30 18:13:02 -0400

    Simplify imgs page.

--- pages/img.js
+++ pages/img.js
@@ -2,23 +2,19 @@
 
 // Frontpage, description is taken from package.json
 
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import Header from '../shared/components/header'
 import { initStore } from '../shared/store'
 import withRematch from '../shared/utils/withRematch'
 import ImageUploader from '../shared/components/image-uploader'
 
-class Img extends Component {
-  render () {
-    return <Fragment>
-      <Header title='Img' />
-      <section className='section'>
-        <div className='container'>
-          <ImageUploader />
-        </div>
-      </section>
-    </Fragment>
-  }
-}
+const Img = () => <Fragment>
+  <Header title='Img' />
+  <section className='section'>
+    <div className='container'>
+      <ImageUploader />
+    </div>
+  </section>
+</Fragment>
 
 export default withRematch(initStore)(Img)
