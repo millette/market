475795c6e89aad29ec05975f1cee16d7f5889d1b
Robin Millette <robin@millette.info>
2018-07-02 17:24:30 -0400

    More about.

--- pages/about.js
+++ pages/about.js
@@ -16,51 +16,48 @@ const About = () => <Fragment>
   <Header title='À propos' subtitle={description.slice(0, -1)} />
   <section className='section'>
     <div className='container'>
-      <div className='content'>
-        <div className='columns'>
-          <div className='column'>
-            <h3 className='title is-3'>Inscription</h3>
-            <p>
-              L’inscription au site est simple et rapide.
-              La seule information exigée est un nom d’utilisateur
-              et un mot de passe pour vous connecter.
-            </p>
-          </div>
-          <div className='column'>
-            <h3 className='title is-3'>Second truc</h3>
-            <p>
-              Une fois connecté, vous pouvez créer votre premier...
-            </p>
-          </div>
-          <div className='column'>
-            <h3 className='title is-3'>El 3e</h3>
-            <p>
-              Quand l’habitude se présente...
-            </p>
-          </div>
+      <div className='columns'>
+        <div className='column'>
+          <h3 className='title is-3'>Inscription</h3>
+          <p>
+            L’inscription au site est simple et rapide.
+          </p>
         </div>
-        <div className='columns'>
-          <div className='column'>
-            <h3 className='title is-3'>Vie privée</h3>
-            <p>
-              Toutes les informations demeurent privées et seulement accessibles
-              au détenteur du compte.
-            </p>
-          </div>
-          <div className='column'>
-            <h3 className='title is-3'>Nous contacter</h3>
-            <p>
-              Utilisez le formulaire pour nous contacter ou envoyez nous un email.
-            </p>
-          </div>
-          <div className='column'>
-            <a target='_blank' href={`https://github.com/${repository}`}>
-              <h3 className='title is-3'>Crédits</h3>
-            </a>
-            <p>
-              Les sources du logiciel utilisé par ce service sont disponibles sur GitHub.
-            </p>
-          </div>
+        <div className='column'>
+          <h3 className='title is-3'>Professeurs</h3>
+          <p>
+            Le professeur peut gérer plusieurs cours à plusieurs sessions.
+          </p>
+        </div>
+        <div className='column'>
+          <h3 className='title is-3'>Étudiants</h3>
+          <p>
+            L’étudiant peut être inscrit à plusieurs cours à la fois
+            et aura aussi accès aux informations des sessions précédentes.
+          </p>
+        </div>
+      </div>
+      <div className='columns'>
+        <div className='column'>
+          <h3 className='title is-3'>Vie privée</h3>
+          <p>
+            Toutes les informations demeurent privées et seulement accessibles
+            au détenteur du compte.
+          </p>
+        </div>
+        <div className='column'>
+          <h3 className='title is-3'>Nous contacter</h3>
+          <p>
+            Utilisez le formulaire pour nous contacter ou envoyez nous un email.
+          </p>
+        </div>
+        <div className='column'>
+          <a target='_blank' href={`https://github.com/${repository}`}>
+            <h3 className='title is-3'>Crédits</h3>
+          </a>
+          <p>
+            Les sources du logiciel utilisé par ce service sont disponibles sur GitHub.
+          </p>
         </div>
       </div>
     </div>
